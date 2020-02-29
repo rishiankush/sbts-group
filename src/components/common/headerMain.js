407d442a0ba0d3f0ecb82191d5d66650b32d865f
Ankush Rishi <ankushrishi92@gmaill.com>
2020-02-29 15:58:29 +0530

    navbar

--- src/components/common/headerMain.js
+++ src/components/common/headerMain.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { withRouter } from 'react-router-dom';
 import SbtsLogo from '../../assets/images/logo/sbtsLogo.png';
+import {Navbar,Nav,NavItem,NavDropdown,MenuItem,Container,Card,Button} from "react-bootstrap";
+  
 
 class MainHeader extends Component {
     constructor(props) {
@@ -15,50 +17,30 @@ class MainHeader extends Component {
             <div className="theme-main-menu theme-menu-one">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4">
+
+                   
+
+                        <div className="col-8 col-md-4">
                             <div className="logo"><a href="index.html"><img style={{width:'150px'}} src={SbtsLogo} alt="" /></a></div>
                         </div>
                         <div className="col-md-8">
-                            <nav id="mega-menu-holder" className="navbar navbar-expand-lg">
-                                <div className="container nav-container">
-                                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                        <i className="flaticon-setup"></i>
-                                    </button>
-                                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                        <ul className="navbar-nav">
-                                            <li className="nav-item active dropdown">
-                                                <a href="index.html" className="nav-link dropdown-toggle" data-toggle="dropdown">Home</a>
-                                            </li>
-                                            <li className="nav-item dropdown position-relative">
-                                                <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Products</a>
-                                                <ul class="dropdown-menu">
-							            <li class="dropdown-submenu dropdown">
-							              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">E-Goverment Suite</a>
-							            </li>
-                                        <li class="dropdown-submenu dropdown">
-							              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Financial Suite</a>
-							            </li>
-                                        <li class="dropdown-submenu dropdown">
-							              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Training Suite</a>
-							            </li>
-                                        <li class="dropdown-submenu dropdown">
-							              	<a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">Call Center Suite</a>
-							            </li>
-						            </ul>
-                                            </li>
-                                            <li className="nav-item dropdown position-relative">
-                                                <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Services</a>
-                                            </li>
-                                            <li className="nav-item dropdown">
-                                                <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Careers</a>
-                                            </li>
-                                            <li className="nav-item dropdown position-relative">
-                                                <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Contact Us</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </nav>
+                        <Navbar collapseOnSelect bg="" variant="light" expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home">Services</Nav.Link>
+                        <NavDropdown title="Products" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">E-Goverment</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Financial</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Training</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Call Center</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#link">Careers</Nav.Link>
+                        <Nav.Link href="#link">Contact Us</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                        </Navbar>
                         </div>
                     </div>
                 </div>
