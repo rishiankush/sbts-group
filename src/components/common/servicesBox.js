5536687aceb74537d75b6f912a3da458be44b608
rishiankush <ankushrishi92@gmail.com>
2020-03-17 10:40:49 +0530

    new changes

--- src/components/common/servicesBox.js
+++ src/components/common/servicesBox.js
@@ -39,41 +39,38 @@ class Servicesbox extends Component {
                     <img src={require("../../assets/images/shape/shape-48.svg")} alt="" class="shape-four" />
                     <img src={require("../../assets/images/shape/shape-49.svg")} alt="" class="shape-five" />
                     <div class="row">
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box support-feature js-tilt">
                                 <div class="icon-box"><img src={internationalconsulting} alt="" /></div>
                                 <h4 class="title">International Consulting</h4>
                             </div>
                         </div>
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box price-feature js-tilt">
                                 <div class="icon-box"><img src={ArtificialIntelligence} alt="" /></div>
                                 <h4 class="title">Artificial Intelligence / Machine Learning</h4>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-2 service-boxes">
                         <div class="feature-offer-box access-feature js-tilt">
                                 <div class="icon-box"><img src={Cybersecurity} alt="" /></div>
                                 <h4 class="title">Cyber Security</h4>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box support-feature js-tilt">
                                 <div class="icon-box"><img src={Telecommunications} alt="" /></div>
                                 <h4 class="title">Telecommunications</h4>
                             </div>
                         </div>
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box price-feature js-tilt">
                                 <div class="icon-box"><img src={ITConsulting} alt="" /></div>
                                 <h4 class="title">IT Consulting</h4>
                             </div>
                         </div>
-                        <div className="col-md-4">
-                        <div class="feature-offer-box access-feature js-tilt">
+                        <div className="col-md-2 service-boxes">
+                            <div class="feature-offer-box access-feature js-tilt">
                                 <div class="icon-box"><img src={Mobileapplicationdevelopment} alt="" /></div>
                                 <h4 class="title">Mobile Application Development</h4>
                             </div>
@@ -81,54 +78,52 @@ class Servicesbox extends Component {
                     </div>
 
                     <div class="row">
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box support-feature js-tilt">
                                 <div class="icon-box"><img src={DIntegration} alt="" /></div>
                                 <h4 class="title">Database Integration</h4>
                             </div>
                         </div>
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box price-feature js-tilt">
                                 <div class="icon-box"><img src={SDevelopment} alt="" /></div>
                                 <h4 class="title">Software Development</h4>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-2 service-boxes">
                         <div class="feature-offer-box access-feature js-tilt">
                                 <div class="icon-box"><img src={HDevelopment} alt="" /></div>
                                 <h4 class="title">Hardware Design and Development </h4>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box support-feature js-tilt">
                                 <div class="icon-box"><img src={Networking} alt="" /></div>
                                 <h4 class="title">Networking</h4>
                             </div>
                         </div>
-                        <div class="col-md-4 col-lg-4">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box price-feature js-tilt">
                                 <div class="icon-box"><img src={VPNWANIntegration} alt="" /></div>
                                 <h4 class="title">VPN/WAN Integration</h4>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-2 service-boxes">
                         <div class="feature-offer-box access-feature js-tilt">
                                 <div class="icon-box"><img src={Enterpriseweb} alt="" /></div>
                                 <h4 class="title">Enterprise Web</h4>
                             </div>
                         </div>
                     </div>
+                    
                     <div class="row">
-                        <div class="col-md-6 col-lg-6">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box support-feature js-tilt">
                                 <div class="icon-box"><img src={Helpdeskintegration} alt="" /></div>
                                 <h4 class="title">Help Desk Integration</h4>
                             </div>
                         </div>
-                        <div class="col-md-6 col-lg-6">
+                        <div class="col-md-2 service-boxes">
                             <div class="feature-offer-box price-feature js-tilt">
                                 <div class="icon-box"><img src={Supporttraining} alt="" /></div>
                                 <h4 class="title">Support/ Training Management</h4>
